14a4ad3b310d2740c4036f3b31efbb7138ed4eb7
hymnis <hymnis@plazed.net>
2016-12-11 15:03:02 +0100

    Updates timer configuration and input arguments.
     * Updates timer configuration path
     * Updates .desktop file, so exec calls python with file as argument
     * Adds input arguments, for log level and showing help
     * Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from distutils.core import setup
 from DistUtilsExtra.command import *
 
-#DEST="/opt/extras.ubuntu.com/steeper/"
-DEST="share/steeper/"
+# DEST="/opt/extras.ubuntu.com/steeper/"
+DEST = "share/steeper/"
+
 
 class my_build_i18n(build_i18n.build_i18n):
     def run(self):
@@ -12,21 +13,28 @@ class my_build_i18n(build_i18n.build_i18n):
 
         df = self.distribution.data_files
 
-        self.distribution.data_files = [(d.replace("share/locale/", DEST+"locale/"), s) for d, s in df]
+        self.distribution.data_files = [
+            (d.replace("share/locale/", DEST + "locale/"), s) for d, s in df]
+
 
 setup(
-      cmdclass = {"build": build_extra.build_extra,
-                  "build_i18n": my_build_i18n},
-      name = "steeper",
-      version = "0.2.1",
-      description = "Simple tea steeping timer (mainly for Unity)",
-      author = "Peter Dahlman",
-      author_email = "peter.dahlman@gmail.com",
-      url = "https://github.com/hymnis/steeper",
-      license = "GNU GPL v3",
-      data_files = [("share/applications/", ["steeper.desktop"]),
-                    (DEST+"icons/", ["icons/steeper-scalable.svg",
-                    "icons/steeper-48x48.png",
-                    "icons/steeper-64x64.png",
-                    "icons/steeper-128x128.png"]),
-                    (DEST, ["window.ui", "steeper.py", "pycanberra.py", "help.txt"])])
+    cmdclass={"build": build_extra.build_extra,
+              "build_i18n": my_build_i18n},
+    name="steeper",
+    version="0.2.2",
+    description="Simple tea steeping timer (mainly for Unity)",
+    author="Peter Dahlman",
+    author_email="peter.dahlman@gmail.com",
+    url="https://github.com/hymnis/steeper",
+    license="GNU GPL v3",
+    data_files=[("share/applications/", ["steeper.desktop"]),
+                (DEST + "icons/", ["icons/steeper-scalable.svg",
+                                   "icons/steeper-48x48.png",
+                                   "icons/steeper-64x64.png",
+                                   "icons/steeper-128x128.png"]),
+                (DEST, [
+                    "window.ui",
+                    "steeper.py",
+                    "pycanberra.py",
+                    "help.txt"])
+                ])
--- steeper.py
+++ steeper.py
@@ -3,25 +3,33 @@
 import time
 import json
 import locale
+import logging
 import os.path
 import pycanberra
+import sys
+import getopt
 
+import gi
+gi.require_version('Unity', '7.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Notify', '0.7')
 from gi.repository import Unity, GObject, Gtk, Notify, Gdk, Pango, GLib
 
 GETTEXT_DOMAIN = "steeper"
-REMIND_DELTA_SECONDS=30
-DATA = "/usr/share/steeper/"
-##DATA = "/home/hymnis/Dropbox/Src/steeper-workdir/steeper-git/"
+REMIND_DELTA_SECONDS = 30
+# DATA = "/usr/share/steeper/"
+DATA = "/home/hymnis/ownCloud/src/steeper-workdir/steeper-git/"
 
 # Use locale instead of gettext, so GTK gets the change
-locale.bindtextdomain(GETTEXT_DOMAIN, DATA+"locale/")
+locale.bindtextdomain(GETTEXT_DOMAIN, DATA + "locale/")
 locale.textdomain(GETTEXT_DOMAIN)
 _ = locale.gettext
+
 
 class Notification(Notify.Notification):
     def __init__(self):
         image = Gtk.Image()
-        image.set_from_file(DATA+"icons/steeper-128x128.png")
+        image.set_from_file(DATA + "icons/steeper-128x128.png")
         icon_pixbuf = image.get_pixbuf()
 
         Notify.Notification.__init__(self)
@@ -36,11 +44,14 @@ class Notification(Notify.Notification):
         if elapsed < 20:
             body = _("finished just now")
         elif elapsed < 60:
-            body = _("finished %s seconds ago") % time.strftime("%S", time.localtime(elapsed))
+            body = _("finished %s seconds ago") % time.strftime(
+                "%S", time.localtime(elapsed))
         else:
-            body = _("finished %s minutes ago") % time.strftime("%M:%S", time.localtime(elapsed))
+            body = _("finished %s minutes ago") % time.strftime(
+                "%M:%S", time.localtime(elapsed))
 
         self.update(_("%s is ready") % timer.obj["name"], body, None)
+
 
 class Timer:
     def __init__(self, obj):
@@ -55,7 +66,8 @@ class Timer:
 
     def start(self):
         if self.obj["brew_toggle"]:
-            self.actual_duraction = self.obj["duration"] + (self.obj["increment"] * self.obj["brew"])
+            self.actual_duraction = self.obj[
+                "duration"] + (self.obj["increment"] * self.obj["brew"])
         else:
             self.actual_duraction = self.obj["duration"]
 
@@ -71,6 +83,7 @@ class Timer:
 
         return progress
 
+
 class TreeView:
     def __init__(self, obj, model):
         self._obj = obj
@@ -81,14 +94,17 @@ class TreeView:
         temperature_range = Gtk.ListStore(str)
 
         for i in range(50, 100):
-            temperature_range.prepend([str(i)+" °C"])
+            temperature_range.prepend([str(i) + " °C"])
 
-        transl = (("name", _("Name")), ("temperature", _("Temp.")), ("duration", _("Duration")), ("increment", _("Increment")), ("brew", _("Brews")), ("brew_toggle", _("Count Brews?")))
+        transl = (
+            ("name", _("Name")),
+            ("temperature", _("Temp.")),
+            ("duration", _("Duration")),
+            ("increment", _("Increment")),
+            ("brew", _("Brews")),
+            ("brew_toggle", _("Count Brews?")))
         cell_align = [0.0, 0.5]
-        col_settings = {"min": 110
-                       ,"max": 200
-                       ,"align": 0.0
-                       }
+        col_settings = {"min": 110, "max": 200, "align": 0.0}
 
         for key, title in transl:
             cell = None
@@ -122,7 +138,7 @@ class TreeView:
                 cell.set_property("editable", True)
                 cell.connect("edited", self._edited_spin, key)
 
-                # value=0, lower=0, upper=0, step_incr=0, page_incr=0, page_size=0
+            # value=0, lower=0, upper=0, step_incr=0, page_incr=0, page_size=0
                 adjustment = Gtk.Adjustment(0, 0, 50, 1, 10, 0)
                 cell.set_property("adjustment", adjustment)
 
@@ -162,12 +178,21 @@ class TreeView:
             self._obj.append_column(col)
 
     def add_addline(self):
-        self._model.append({"name": _("New Entry"), "temperature": "70 °C", "duration": 0, "increment": 30, "brew": "-", "brew_toggle": False})
+        self._model.append({
+            "name": _("New Entry"),
+            "temperature": "70 °C",
+            "duration": 0,
+            "increment": 30,
+            "brew": "-",
+            "brew_toggle": False
+        })
 
     def in_edit(self):
         return any([c.get_property("editing") for c in self._cells])
 
     def _toggled_cb(self, cell, itr, key):
+        logging.debug("TreeView: Toggled CB")
+
         status = cell.get_active()
         status = False if status else True
 
@@ -183,9 +208,13 @@ class TreeView:
         self._model[itr][key] = status
 
     def _edited_combo(self, cell, itr, value, key):
+        logging.debug("TreeView: Edited COMBO")
+
         self._model[itr][key] = value
 
     def _edited_cb(self, cell, itr, value, key):
+        logging.debug("TreeView: Edited CB")
+
         # Allow different input formats
         formats = ["%M", "%M:%S", "%M.%S", "%H:%M:%S"]
 
@@ -199,13 +228,14 @@ class TreeView:
                 except:
                     continue
 
-            if t is None: return
+            if t is None:
+                return
 
-            value = t.tm_sec + 60 * t.tm_min + 60*60 * t.tm_hour
+            value = t.tm_sec + 60 * t.tm_min + 60 * 60 * t.tm_hour
         elif key == "temperature":
             pass
-        else:
-            value = value#.decode("utf-8") # for consistency, obsolete in python3??
+        # else:
+            # value = value
 
         self._model[itr][key] = value
 
@@ -215,6 +245,8 @@ class TreeView:
             self.add_addline()
 
     def _edited_spin(self, cell, itr, value, key):
+        logging.debug("TreeView: Edited SPIN")
+
         if key == "brew" and self._model[itr][key] != "-":
             self._model[itr][key] = int(value)
 
@@ -222,17 +254,20 @@ class TreeView:
         try:
             v = model[itr][0][key]
         except KeyError as e:
-            print("[error] KeyError - missing '"+ key +"' in data list (from file)")
+            print(
+                "[error] KeyError - missing '" +
+                key + "' in data list (from file)." +
+                " (" + e + ")")
             v = False
 
         if key == "duration" or key == "increment":
-            if v >= 60*60:
+            if v >= 60 * 60:
                 v = time.strftime("%H:%M:%S", time.gmtime(v))
             else:
                 v = time.strftime("%M:%S", time.gmtime(v))
         elif key == "brew":
             v = str(v)
-            
+
             if v == "-":
                 cell.set_property("editable", False)
             else:
@@ -245,11 +280,13 @@ class TreeView:
         if key == "brew_toggle":
             cell.set_property("active", v)
         else:
-            cell.set_property("style", Pango.Style.ITALIC if last else Pango.Style.NORMAL)
+            cell.set_property(
+                "style", Pango.Style.ITALIC if last else Pango.Style.NORMAL)
             cell.set_property("text", v)
 
+
 class ListStore:
-    FILE = GLib.get_user_data_dir() + "/steeper.js"
+    FILE = GLib.get_user_data_dir() + "/steeper/steeper.json"
 
     def __init__(self, obj):
         self._obj = obj
@@ -258,7 +295,10 @@ class ListStore:
 
     def load(self):
         try:
-            f = open(self.FILE)
+            if not os.path.isdir(os.path.dirname(self.FILE)):
+                os.mkdir(os.path.dirname(self.FILE))
+
+            f = open(self.FILE, "r+")
 
             for t in json.load(f):
                 self.append(t)
@@ -268,20 +308,30 @@ class ListStore:
             f.close()
 
     def save(self):
-        f = open(self.FILE, "w")
+        logging.debug("ListStore: Save")
 
-        json.dump([t[0] for t in self._obj][0:-1], f)
+        try:
+            f = open(self.FILE, "w")
 
-        f.close()
+            json.dump([t[0] for t in self._obj][0:-1], f)
+        except IOError as e:
+            print("IOError: ", e)
+        except:
+            pass
+        else:
+            f.close()
 
     def __getitem__(self, k):
+        logging.debug("ListStore: GetItem")
         return self._obj[k][0]
 
     def __setitem__(self, k, v):
+        logging.debug("ListStore: SetItem")
         self._obj[k][0] = v
 
     def append(self, v):
         self._obj.append((v,))
+
 
 class Controller:
     def __init__(self):
@@ -296,22 +346,27 @@ class Controller:
         xml.set_translation_domain(GETTEXT_DOMAIN)
         xml.add_from_file(DATA + "window.ui")
 
-        xml.connect_signals({"hide-widget": lambda w, *args: w.hide_on_delete()})
+        xml.connect_signals({
+            "hide-widget": lambda w,
+            *args: w.hide_on_delete()})
 
         # Quit
-        xml.get_object("menuitem_quit").connect("activate", lambda *args: self.main.quit())
+        xml.get_object("menuitem_quit").connect(
+            "activate", lambda *args: self.end())
 
         # Help
         self.helpWnd = xml.get_object("helpdialog1")
-        xml.get_object("menuitem_help").connect("activate", lambda *args: self.show_help())
+        xml.get_object("menuitem_help").connect(
+            "activate", lambda *args: self.show_help())
 
         # Don't show help item if we are missing the help document
-        if os.path.isfile(DATA+"help.txt") == False:
+        if os.path.isfile(DATA + "help.txt") is False:
             xml.get_object("menuitem_help").hide()
 
         # About
         aboutWnd = xml.get_object("aboutdialog1")
-        xml.get_object("menuitem_about").connect("activate", lambda *args: aboutWnd.show())
+        xml.get_object("menuitem_about").connect(
+            "activate", lambda *args: aboutWnd.show())
 
         self.le = Unity.LauncherEntry.get_for_desktop_file("steeper.desktop")
 
@@ -360,22 +415,24 @@ class Controller:
 
         self.sel = int(str(self.sel))
 
-        self.start_button.set_sensitive(not (self.sel == len(self.store._obj) - 1))
+        self.start_button.set_sensitive(
+            not (self.sel == len(self.store._obj) - 1))
 
     def on_button_click(self, *a):
-        # Start timer if it's off and stop if it's on
+        '''Start/stop time'''
         if self.timer is None:
             try:
                 self.start()
-            except ValueError: # gets thrown by timer if duration == 0
+            except ValueError:  # gets thrown by timer if duration == 0
                 pass
         else:
             self.stop()
 
     def show_help(self, *a):
-        # Get dialog information from glade file, populate and then display it
-        # If we actually have a help file to get data from
-        if os.path.isfile(DATA+"help.txt"):
+        '''Show help dialog
+        Get dialog information from glade file, populate and then display it
+        If we actually have a help file to get data from'''
+        if os.path.isfile(DATA + "help.txt"):
             xml = Gtk.Builder()
             xml.set_translation_domain(GETTEXT_DOMAIN)
             xml.add_from_file(DATA + "window.ui")
@@ -386,30 +443,36 @@ class Controller:
 
             # Help close button
             help_close_button = xml.get_object("button2")
-            help_close_button.connect("clicked", lambda *args: self.helpWnd.hide())
+            help_close_button.connect(
+                "clicked", lambda *args: self.helpWnd.hide())
 
-            help_text = open(DATA+"help.txt", "r")
-            h_tag = self.help_buffer.create_tag("h", size_points=16, weight=Pango.Weight.BOLD)
-            i_tag = self.help_buffer.create_tag("i", style=Pango.Style.ITALIC)
-            b_tag = self.help_buffer.create_tag("b", weight=Pango.Weight.BOLD)
+            help_text = open(DATA + "help.txt", "r")
+            h_tag = self.help_buffer.create_tag(
+                "h", size_points=16, weight=Pango.Weight.BOLD)
+            i_tag = self.help_buffer.create_tag(
+                "i", style=Pango.Style.ITALIC)
+            b_tag = self.help_buffer.create_tag(
+                "b", weight=Pango.Weight.BOLD)
 
             for line in help_text:
                 position = self.help_buffer.get_end_iter()
 
                 if str(line)[:1] == "=":  # Headline
                     line = str(line)[1:]
-                    self.help_buffer.insert_with_tags(position, str(line)+"\n", h_tag)
+                    self.help_buffer.insert_with_tags(
+                        position, str(line) + "\n", h_tag)
                 elif str(line)[:1] == "*":  # Bold first word
                     line = str(line)[1:]
                     parts = line.split(" ", 1)
 
-                    if parts[0] != None:
-                        self.help_buffer.insert_with_tags(position, str(parts[0]), b_tag)
+                    if parts[0] is not None:
+                        self.help_buffer.insert_with_tags(
+                            position, str(parts[0]), b_tag)
                     if len(parts) == 2:
                         line = parts[1]
-                        self.help_buffer.insert(position, str(line)+"\n")
+                        self.help_buffer.insert(position, str(line) + "\n")
                 else:  # Normal
-                    self.help_buffer.insert(position, str(line)+"\n")
+                    self.help_buffer.insert(position, str(line) + "\n")
 
             self.helpWnd.show()
 
@@ -423,6 +486,7 @@ class Controller:
         self.label.set_text(_("%s: %s remaining") % (name, remaining))
 
     def start(self):
+        '''Start timer'''
         self.timer = Timer(self.store[self.sel])
         self.timer.start()
         self.tick_src = GObject.timeout_add_seconds(1, self.do_tick)
@@ -438,6 +502,7 @@ class Controller:
         self.window.iconify()
 
     def stop(self):
+        '''Stop timer'''
         self.le.set_property("urgent", False)
         self.le.set_property("progress_visible", False)
         self.start_button.set_label(_("Start Timer"))
@@ -469,7 +534,8 @@ class Controller:
     def start_notification_loop(self):
         self.seen = False
         self.show_notification()
-        self.notify_src = GObject.timeout_add_seconds(REMIND_DELTA_SECONDS, self.show_notification)
+        self.notify_src = GObject.timeout_add_seconds(
+            REMIND_DELTA_SECONDS, self.show_notification)
 
     def do_tick(self):
         p = self.timer.get_progress()
@@ -488,6 +554,8 @@ class Controller:
         return p < 1.0
 
     def end(self, *a):
+        '''End process
+        Stop timer, save data and then quit'''
         self.stop()
         self.store.save()
         self.main.quit()
@@ -514,7 +582,7 @@ class Controller:
 
         # Get value from store
         itr = self.store._obj.get_iter(self.sel)
-        item = self.store._obj.get_value(itr,0)
+        item = self.store._obj.get_value(itr, 0)
 
         if not item["brew_toggle"]:
             return False
@@ -529,8 +597,42 @@ class Controller:
         # Set new value and update TreeView
         item["brew"] = newValue
         current = self.list._obj.get_cursor()
-        self.list._obj.set_cursor(current[0]) # trigger treeview update (focus row)
+        self.list._obj.set_cursor(current[0])  # trigger treeview update
+
+
+def main(argv):
+    help_txt = "Options:\n -h, --help\t\t\tShow this help" \
+        "\n -l <level>, --log=<level>\tSet log level"
+    loglevel = "WARNING"  # default value
+
+    try:
+        opts, args = getopt.getopt(argv, "hl:", ["help", "log="])
+    except getopt.GetoptError:
+        print(help_txt)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(help_txt)
+            sys.exit()
+        elif opt in ("-l", "--log"):
+            loglevel = arg
+
+    try:
+        numeric_level = getattr(logging, loglevel.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % loglevel)
+    except ValueError as e:
+        print(e)
+        sys.exit(2)
+
+    logging.basicConfig(level=numeric_level)
+
+    logging.debug("Debug logging is ON")
+
 
 if __name__ == "__main__":
+    main(sys.argv[1:])
+
     c = Controller()
     c.run()
